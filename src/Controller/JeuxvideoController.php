4f50f84032ef20761db47a2977cea2b14f8bd259
duncanidaho14 <elhadibeddarem@gmail.com>
2021-09-03 17:27:23 +0200

    page mes jeux

--- src/Controller/JeuxvideoController.php
+++ src/Controller/JeuxvideoController.php
@@ -2,20 +2,81 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\Jeuxvideo;
+use App\Form\JeuxvideoType;
+use App\Repository\UserRepository;
+use App\Repository\JeuxvideoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\JeuxvideoRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class JeuxvideoController extends AbstractController
 {
     /**
-     * @Route("/jeuxvideo", name="jeuxvideo")
+     * @Route("/mes-jeux/", name="jeuxvideo")
+     * 
      */
-    public function index(): Response
+    public function index(JeuxvideoRepository $jeuxvideoInstance): Response
     {
-        return $this->render('jeuxvideo/index.html.twig', [
-            'controller_name' => 'JeuxvideoController',
+        return $this->render('jeuxvideo/mesjeux.html.twig', [
+            'jeuxvideo' => $jeuxvideoInstance->findBy(array('user' => $this->getUser()))
+            
+        ]);
+    }
+
+    /**
+     * @Route("/creation/jeuxvideo", name="create_article")
+     * @IsGranted("ROLE_USER")
+     */
+    public function createGame(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $jeuxvideo = new Jeuxvideo();
+        $form = $this->createForm(JeuxvideoType::class, $jeuxvideo);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $jeuxvideo->setUser($this->getUser());
+            $entityManager->persist($jeuxvideo);
+            $entityManager->flush();
+            $this->addFlash(
+                'success',
+                'Le jeux video a bien été crée ! '
+            );
+        }
+        return $this->render('jeuxvideo/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/jeuxvideo/{slug}/edition", name="edit_article")
+     * @Security("is_granted('ROLE_USER') and user === jeuxvideo.getUser()", message="Ce jeux video ne vous appartient pas, vous ne pouvez pas le modifier")
+     */
+    public function editGame(Request $request, Jeuxvideo $jeuxvideo, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(JeuxvideoType::class, $jeuxvideo);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $entityManager->persist($jeuxvideo);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Vous avez bien éditer votre jeux : ' . $jeuxvideo->getName() . ' !'
+            );
+        }
+
+        return $this->render('jeuxvideo/edit.html.twig', [
+            'form' => $form->createView(),
+            'jeuxvideo' => $jeuxvideo
         ]);
     }
 
@@ -25,8 +86,25 @@ class JeuxvideoController extends AbstractController
     public function displayJeuxvideo(JeuxvideoRepository $jeuxvideoRepository, $slug): Response
     {
 
-        return $this->render('jeuxvideo/index.html.twig', [
+        return $this->render('jeuxvideo/show.html.twig', [
             'jeuxvideo' => $jeuxvideoRepository->findOneBySlug($slug)
         ]);
+    }
+
+    /**
+     * @Route("/jeuxvideo/{slug}/delete", name="article_delete")
+     * @Security("is_granted('ROLE_USER') and user == jeuxvideo.getUser()", message="Vous n'avez pas le droit d'accéder à ce jeux")
+     */
+    public function delete(Jeuxvideo $jeuxvideo, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($jeuxvideo);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'success',
+            "Le jeux <strong>{$jeuxvideo->getName()}</strong> a bien été supprimée ! "
+        );
+
+        return $this->redirectToRoute("accueil");
     }
 }
---
+++ src/Form/JeuxvideoType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Jeuxvideo;
+use Symfony\Component\Form\AbstractType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class JeuxvideoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('imageFile', VichFileType::class)
+            ->add('price', MoneyType::class)
+            ->add('description', TextareaType::class)
+            ->add('categories')
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Jeuxvideo::class,
+        ]);
+    }
+}
