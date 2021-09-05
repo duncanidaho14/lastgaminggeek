17c5f3f76040f8ba802ad3c5d2f4cf5bf597daa2
duncanidaho14 <elhadibeddarem@gmail.com>
2021-09-06 00:34:54 +0200

    Stripe session id, page success

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Stripe\Stripe;
 use App\Entity\Order;
-
 use App\Classes\Basket;
 use App\Entity\Carrier;
 use App\Entity\Jeuxvideo;
@@ -18,15 +17,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StripeController extends AbstractController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/commande/create-session/{reference}", name="stripe_create_session")
      */
-    public function index(Basket $basket, EntityManagerInterface $entityManager, $reference)
+    public function index(Basket $basket, $reference)
     {
         
         $productsStripe = [];
         $myDomaine = "http://127.0.0.1:8000";
-        $order = $entityManager->getRepository(Order::class)->findOneByReference($reference);
+        $order = $this->entityManager->getRepository(Order::class)->findOneByReference($reference);
         
         if (!$order) {
             new JsonResponse(['error' => 'order']);
@@ -35,7 +41,7 @@ class StripeController extends AbstractController
         
 
         foreach ($order->getOrderDetails()->getValues() as $product) {
-            $productOrder = $entityManager->getRepository(Jeuxvideo::class)->findOneByName($product->getProduct());
+            $productOrder = $this->entityManager->getRepository(Jeuxvideo::class)->findOneByName($product->getProduct());
             $productsStripe[] = [
                 'price_data' => [
                     'currency' => 'eur',
@@ -65,7 +71,7 @@ class StripeController extends AbstractController
 
         header('Content-Type: application/json');
 
-        $checkoutSession = Session::create([
+        $checkout_session = Session::create([
             'customer_email' => $this->getUser()->getEmail(),
             'submit_type' => 'donate',
             'billing_address_collection' => 'required',
@@ -83,27 +89,26 @@ class StripeController extends AbstractController
             'cancel_url' => $myDomaine . '/commande/cancel/{CHECKOUT_SESSION_ID}'
         ]);
 
-        //$order->setStripeSessionId($checkoutSession->id);
-        $entityManager->flush();
+        $order->setStripeSessionId($checkout_session->id);
+        $this->entityManager->flush();
 
-        $response = new JsonResponse(['location' => $checkoutSession->url, 'id' => $checkoutSession->id]);
-        
-        
-
+        $response = new JsonResponse(['location' => $checkout_session->url, 'id' => $checkout_session->id]);
+        \json_decode($response);
         return $response;
-                
     }
 
     /**
-     * @Route("/success/{stripeSessionId}", name="stripe_success")
+     * @Route("/commande/success/{stripeSessionId}", name="stripe_success")
      */
     public function stripeSuccess($stripeSessionId): Response
     {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByStripeSessionId($stripeSessionId);
+        dd($order);
         return $this->render('stripe/success.html.twig', []);
     }
 
     /**
-     * @Route("/cancel/{stripeSessionId}", name="stripe_cancel")
+     * @Route("/commande/cancel/{stripeSessionId}", name="stripe_cancel")
      */
     public function stripeCancel($stripeSessionId): Response
     {
