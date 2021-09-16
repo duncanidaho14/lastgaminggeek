8b9f77de9283599416e78d365a6574d6eb1f55e9
duncanidaho14 <elhadibeddarem@gmail.com>
2021-09-16 21:56:04 +0200

    install phpunit-bridge

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Controller;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    /** @test */
+    public function homepage_should_display_all_game()
+    {
+        $client = static::createClient();
+        
+        $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        
+
+    }
+}
