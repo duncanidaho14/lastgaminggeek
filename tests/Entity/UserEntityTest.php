681ddcc6fea505bc27b1e45aa5d52a1b649c3ecb
duncanidaho14 <elhadibeddarem@gmail.com>
2021-10-14 14:38:52 +0200

    meddy recup

--- tests/Entity/UserEntityTest.php
+++ tests/Entity/UserEntityTest.php
@@ -4,11 +4,13 @@ namespace App\Tests\Entity;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Entity\User;
+use Symfony\Component\Validator\ConstraintViolationList;
 
 class UserEntityTest extends KernelTestCase
 {
     private const EMAIL_CONSTRAINT_MESSAGE = 'L\'email n\'est pas valide';
-    private const NOT_BLANK_MESSAGE = "Veuillez saisir une valeur";
+    private const NOT_BLANK_CONSTRAINT_MESSAGE = "Veuillez saisir une valeur";
     private const INVALID_EMAIL_VALUE = "elhadibeddarem@gmail";
     private const VALID_EMAIL_VALUE = "elhadibeddarem@gmail.com";
     private const VALID_PASSWORD_VALUE = "password";
@@ -20,4 +22,46 @@ class UserEntityTest extends KernelTestCase
         $this->validator = $kernel->getContainer()->get('validator');
         
     }
+
+    public function testUserEntityIsValid(): void
+    {
+        $user = new User();
+        $user->setEmail(self::VALID_EMAIL_VALUE)
+            ->setPassword(self::VALID_PASSWORD_VALUE)
+        ;
+        $this->getValidationErrors($user, 4);
+    }
+
+    public function testUserEntityIsInvalidBecauseNoEmailEntered(): void
+    {
+        $user = new User();
+        
+        $user->setPassword(self::VALID_PASSWORD_VALUE);
+        
+        $errors = $this->getValidationErrors($user, 5);
+
+        $this->assertEquals(self::NOT_BLANK_CONSTRAINT_MESSAGE, $errors[0]->getMessage());
+
+    }
+
+    public function testUserEntityIsInvalidBecauseNoPasswordEntered(): void
+    {
+        $user = new User();
+        
+        $user->setEmail(self::VALID_EMAIL_VALUE);
+        
+        $errors = $this->getValidationErrors($user, 5);
+
+        $this->assertEquals(self::NOT_BLANK_CONSTRAINT_MESSAGE, $errors[0]->getMessage());
+
+    }
+
+    private function getValidationErrors(User $user, int $numberOfExpectedErrors): ConstraintViolationList
+    {
+        $errors = $this->validator->validate($user);
+        $this->assertCount($numberOfExpectedErrors, $errors);
+        return $errors;
+    }
+
+    
 }
