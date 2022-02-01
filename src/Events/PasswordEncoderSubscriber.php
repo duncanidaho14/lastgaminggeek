88cd97dc87b1fef87356f91bf195e8c158edf8b6
duncanidaho14 <elhadibeddarem@gmail.com>
2022-02-01 14:56:30 +0100

    debug namespace prod passwordencodersubscriber

--- src/Events/PasswordEncoderSubscriber.php
+++ src/Events/PasswordEncoderSubscriber.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class PasswordEncoderSubscriber implements EventSubscriberInterface
 {
-    private UserPasswordEncoderInterface $encoder;
+    private $encoder;
 
     public function __construct(UserPasswordEncoderInterface $encoder)
     {
