b7b6be43613799a528f7609fb2929e2bcc1d5561
duncanidaho14 <elhadibeddarem@gmail.com>
2022-02-03 01:31:14 +0100

    jwt decorator

--- src/OpenApi/JwtDecorator.php
+++ src/OpenApi/JwtDecorator.php
@@ -2,7 +2,20 @@
 
 namespace App\OpenApi;
 
+use ApiPlatform\Core\OpenApi\OpenApi;
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+
 class JwtDecorator
 {
-    
+    private $decorated;
+
+    public function __construct(OpenApiFactoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function __invoke(array $context = []): OpenApi
+    {
+        
+    }
 }
