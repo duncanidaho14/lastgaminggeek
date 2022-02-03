72b34a1276b5ee1924e70aecf1bfa2e55d789b13
duncanidaho14 <elhadibeddarem@gmail.com>
2022-02-03 10:46:57 +0100

    jwtdecorator

--- src/OpenApi/JwtDecorator.php
+++ src/OpenApi/JwtDecorator.php
@@ -9,18 +9,18 @@ use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
 class JwtDecorator implements OpenApiFactoryInterface
 {
     private OpenApiFactoryInterface $decorated;
-    private string $appVersion;
+    //private string $appVersion;
 
-    public function __construct(OpenApiFactoryInterface $decorated, string $appVersion)
+    public function __construct(OpenApiFactoryInterface $decorated)
     {
         $this->decorated = $decorated;
-        $this->appVersion = $appVersion;
+        //$this->appVersion = $appVersion;
     }
 
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = $this->decorated->__invoke($context);
-        
+
         return $openApi;
     }
 }
