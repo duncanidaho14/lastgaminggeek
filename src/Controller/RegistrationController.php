d3f19ff8b6a679b8a7554e461d47d10b9d7d5ac5
duncanidaho14 <elhadibeddarem@gmail.com>
2022-06-22 19:36:04 +0200

    debug version dependancies

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,7 +16,6 @@ use Symfony\Component\Routing\Annotation\Route;
 use SendinBlue\Client\Model\CreateEmailCampaign;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
@@ -31,7 +30,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/inscription", name="app_register")
      */
-    public function register(Request $request, UserPasswordHasherInterface $passwordEncoder, ManagerRegistry $doctrine): Response
+    public function register(Request $request, UserPasswordHasherInterface  $passwordEncoder, ManagerRegistry $doctrine): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -50,39 +49,10 @@ class RegistrationController extends AbstractController
             $entityManager = $doctrine->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
-
-                    # Instantiate the client\
-        // SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey("api-key", "YOUR_API_V3_KEY");
-        // $api_instance = new SendinBlue\Client\Api\EmailCampaignsApi();
-        // $emailCampaigns = new \SendinBlue\Client\Model\CreateEmailCampaign();
-        // # Define the campaign settings\
-        // $email_campaigns['name'] = "Campaign sent via the API";
-        // $email_campaigns['subject'] = "My subject";
-        // $email_campaigns['sender'] = array("name", "From name", "email","elhadibeddarem@gmail.com");
-        // $email_campaigns['type'] = "classic";
-        // # Content that will be sent\
-        // $htmlContent = "Congratulations! You successfully sent this example campaign via the Sendinblue API.";
-        // # Select the recipients\
-        // $recipient =array("listIds"=> [2, 7]);
-        // # Schedule the sending in one hour\
-        // $dateofExpedition = "2018-01-01 00:00:01";
-        // # Make the call to the client\
-        // try {
-        // $result = $api_instance->createEmailCampaign($emailCampaigns);
-        // print_r($result);
-        // } catch (Exception $e) {
-        // echo 'Exception when calling EmailCampaignsApi->createEmailCampaign: ', $e->getMessage(), PHP_EOL;
-        // }
-            // // generate a signed url and email it to the user
-        //     $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-        //         (new TemplatedEmail())
-        //             ->from(new Address('kiruazoldyk19@gmail.com', 'GGEEK'))
-        //             ->to($user->getEmail())
-        //             ->subject('Please Confirm your Email')
-        //             ->htmlTemplate('registration/confirmation_email.html.twig')
-        //     );
-        //     // do anything else you need here, like send an email
-
+            $this->addFlash(
+                'success',
+                'Votre enregistrement a bien été effectué, veuillez vous connecter !'
+            );
             return $this->redirectToRoute('accueil', array('id' => 1));
         }
 
